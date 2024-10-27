7831a61bbdc2dff513a2fabba810b0f8550cc2d1
ShikuSteve <wanjikushalom06@gmail.com>
2024-10-27 11:59:50 +0300

    cors

--- app.js
+++ app.js
@@ -18,6 +18,13 @@ mongoose.connect("mongodb://localhost:27017/passkey", {
   useUnifiedTopology: true,
 });
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 app.use(
   session({
     secret: "keyboard cat",
@@ -155,7 +162,7 @@ app.post("/registerResponse", async (req, res) => {
     console.log("Authenticator:", authenticator);
     // Verify the credential
 
-    const { verified } =await verifyAuthenticationResponse({
+    const { verified } = await verifyAuthenticationResponse({
       response,
       expectedChallenge,
       expectedOrigin,
