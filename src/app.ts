8b2bfa2d60a5d338903bcb8a15a3b50531ae5fc5
ShikuSteve <wanjikushalom06@gmail.com>
2024-11-03 12:18:29 +0300

    fixed the cors

--- src/app.ts
+++ src/app.ts
@@ -32,7 +32,7 @@ mongoose.connect(uri, {
 app.use((req: Request, res: Response, next: NextFunction) => {
   res.setHeader(
     "Access-Control-Allow-Origin",
-    "https://passkey-demos.onrender.com/"
+    "https://passkey-demos.onrender.com"
   );
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
